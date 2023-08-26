1f844303d8f3d4d9b42ddc7498d6052655679a0f
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-08-26 07:29:48 +0530

    second of the day

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <a href="https://paste.sh/AhenWkel#VMs93rnxL4CdThtorVPX9XPo" style={{color:"black",fontSize:"larger"}}> click here to see answer DATE 25-aug-2023</a>
+    <a href="https://paste.sh/3ct0o9Oy#WbG9H9uq7bZVfL4QaSOXDSzS" style={{color:"black",fontSize:"larger"}}> click here to see answer DATE :  26-aug-2023</a>
   </React.StrictMode>,
 )
