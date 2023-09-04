77d08f1af4cdb20f437a2db436cf041e7be8f61b
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-09-04 09:34:16 +0530

    04-sep-2023

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,36 @@ import React from 'react'
 const answer=
 `
 
-   if(root1 == null && root2 == null)
-        {
-            return true;
-        }
-
-        if(root1 == null && root2!= null)
-        return false;
-
-        if(root2 == null && root1 != null)
-        return false;
-
-        if(root1.data != root2.data)
-        {
-            return false;
-        }
-
-        return (isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right) || 
-        isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left));
-
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if((i==0 ||i==n-1 || j==0||j==m-1)&& a[i][j]=='O')  dfs(a,i,j);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                a[i][j]=a[i][j]!='k'?'X':'O';          
+        return a;
+    }
+    static void dfs(char mat[][],int i,int j)
+    {
+        if(i<0 || j<0 || i>=mat.length || j>=mat[0].length || mat[i][j]!='O')
+            return;
+        mat[i][j]='k';
+        dfs(mat,i+1,j);
+        dfs(mat,i-1,j);
+        dfs(mat,i,j+1);
+        dfs(mat,i,j-1);
+    
 `
 const stepp=`
 
 STEP 1: change language to java
 STEP 2: paste at marked position
-class Solution  
-{ 
-    // Return True if the given trees are isomotphic. Else return False.
-    boolean isIsomorphic(Node root1, Node root2)  
-    { 
-         // code here.
-         //paste here
-    }
-    
-}    
+class Solution{
+    static char[][] fill(int n, int m, char a[][])
+    {
+        // code here
+        //paste here
+    } 
+}   
 
 `
 const copyHandler= ()=>{
@@ -62,8 +58,8 @@ const App = () => {
  return (
     <>
         <h1>Solution of POTD  </h1>
-        <h3>DATE:03-sep-2023</h3>
-        <p>at 00:50 am</p>
+        <h3>DATE:04-sep-2023</h3>
+        <p>at 09:36 am</p>
         
         <div>
             <pre>
