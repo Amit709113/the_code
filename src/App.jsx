1df79b3b9ecae0f96ba618fead0fa9aa2dbb47af
Amit Kumar Sahu <sahuamitkumr7091@gmail.com>
2023-10-12 22:23:49 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -3,80 +3,66 @@ import React, { useState } from 'react'
 
 const answer=
 `
-         HashMap<Node,Node>mp=new HashMap<>();
-        Queue<Node>q=new LinkedList<>();
-        q.add(root);
-        while(q.size()!=0){
-            Node temp=q.remove();
-            if(temp.left!=null){
-                mp.put(temp.left,temp);
-                q.add(temp.left);
-            }
-            if(temp.right!=null){
-                mp.put(temp.right,temp);
-                q.add(temp.right);
-            }
-        }
-        ArrayList<Integer>ans=new ArrayList<>();
-        HashMap<Node,Integer>isVisit=new HashMap<>();
-        Queue<Node>q1=new LinkedList<>();
-        Node node=SearchTarget(root,target);
-        q1.add(node);
-        isVisit.put(node,0);
-        while(q1.size()!=0){
-            Node temp=q1.poll();
-            
-            if(temp.left!=null && !isVisit.containsKey(temp.left)){
-                Node left=temp.left;
-                q1.add(left);
-                isVisit.put(left,isVisit.get(temp)+1);
-                if(isVisit.get(left)==k){
-                    ans.add(left.data);
-                }
-            }
-            
-            if(temp.right!=null && !isVisit.containsKey(temp.right)){
-                Node right=temp.right;
-                q1.add(right);
-                isVisit.put(right,isVisit.get(temp)+1);
-                if(isVisit.get(right)==k){
-                    ans.add(right.data);
-                }
-            }
-            
-            if(mp.containsKey(temp) && !isVisit.containsKey(mp.get(temp))){
-                Node parent=mp.get(temp);
-                q1.add(parent);
-                isVisit.put(parent,isVisit.get(temp)+1);
-                if(isVisit.get(parent)==k){
-                    ans.add(parent.data);
-                }
-            }
-            
-        }
-        Collections.sort(ans);
-        return ans;
+Map<String, Integer> mp = new HashMap<>();
+    
+String recur (Node root){
+    if(root ==null){
+        return "$";
     }
-      public static Node SearchTarget(Node root,int val){
-        if(root==null)return null;
-        if(root.data==val)return root;
-        Node left=SearchTarget(root.left,val);
-        Node right=SearchTarget(root.right,val);
-        if(left==null)return right;
-        return left;
+    String s = "";
+    if(root.left==null && root.right==null){
+        s+=root.data;
+        return s;
+    }
+    
+    
+   
+   s+=root.data;
+    s+=recur(root.left);
+    s+=recur(root.right);
+    
+ 
+     mp.put(s,mp.getOrDefault(s,0)+1);
+     
+     return s;
+    
+    
+}
+int dupSub(Node root) {
+
+    recur(root);
+    for(Integer num : mp.values()){
+        
+        if(num >=2){
+            return 1;
+        }
+        
+    
+        
+    }
+    
+    return 0;
+}
+
 `
 const stepp=`
 
         STEP 1: change language to java   
-        
-        STEP 2: paste at marked position
-        class Solution
-        {
-            public static ArrayList<Integer> KDistanceNodes(Node root, int target , int k)
-            {
-                // return the sorted list of all nodes at k dist
-                //paste here
+        Step 2: 
+        class Solution {
+            int dupSub(Node root) {
+                // code here 
             }
+        }
+        remove this from above code
+        
+        int dupSub(Node root ){
+            //code here
+        }
+
+        Step 3: then paste here
+        class Solution{
+            //paste here
         }
         
         
@@ -105,7 +91,7 @@ const App = () => {
  return (
     <>
         <h1>Solution of POTD  </h1>
-        <h3>last modified :  10-oct-2023 9:42 PM</h3>
+        <h3>last modified :  12-oct-2023 10:24 PM</h3>
         
         
         <div>
